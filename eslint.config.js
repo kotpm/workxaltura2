3f1b7ba7c1b4a513d00b5d0b9259cfd4cb7ebbd9
kotpm <kotpm04@ukr.net>
2024-10-27 12:34:46 +0200

    Testing GitHub Actions run

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,vue}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,vue}"] },
+  { 
+    languageOptions: { 
+      globals: { 
+        ...globals.browser,
+        ...globals.jest   // Додає підтримку глобальних змінних Jest
+      } 
+    }
+  },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
 ];
